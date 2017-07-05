77e28079306477bb735ce0ac2e278f1ba762e2a2
Dave Strus <dave@getfretless.com>
2017-07-05 14:58:15 -0400

    Import images in Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
 import './Sidebar.css'
+import quill from './quill.svg'
+import newHover from './new-hover.png'
+import newIcon from './new.png'
 
 const Sidebar = () => {
-  return <p>SIDEBAR!</p>
+  return (
+    <nav className="Sidebar">
+      <div className="logo">
+        <img src={quill} alt="Noteherder" />
+      </div>
+      <a className="new-note" href="/notes">
+        <img
+          src={newHover}
+          alt="New note"
+        />
+        <img
+          className="outline"
+          src={newIcon}
+          alt="New note"
+        />
+      </a>
+      <div className="SignOut">
+        <button>
+          <i className="fa fa-sign-out"></i>
+        </button>
+      </div>
+    </nav>
+  )
 }
 
 export default Sidebar
