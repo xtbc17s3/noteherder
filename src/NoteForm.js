11c1883080e944149811a489bb5a412a06f084de
Dave Strus <dave@getfretless.com>
2017-07-05 15:34:25 -0400

    Style NoteList and NoteForm

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -4,7 +4,26 @@ import './NoteForm.css'
 
 class NoteForm extends Component {
   render() {
-    return <p>NOTE FORM</p>
+    return (
+      <div className="NoteForm">
+        <div className="form-actions">
+          <button type="button">
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </div>
+        <form>
+          <p>
+            <input
+              type="text"
+              name="title"
+              placeholder="Title your note"
+            />
+          </p>
+          
+          <textarea name="body"></textarea>
+        </form>
+      </div>
+    )
   }
 }
 
