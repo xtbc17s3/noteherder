7a330ff30ca64287f0992c79d48c2c70124dbc55
Dave Strus <dave@getfretless.com>
2017-07-06 15:36:11 -0400

    Use an object spread for data props

--- src/App.js
+++ src/App.js
@@ -56,11 +56,15 @@ class App extends Component {
       removeCurrentNote: this.removeCurrentNote,
     }
 
+    const noteData = {
+      notes: this.state.notes,
+      currentNote: this.state.currentNote,
+    }
+
     return (
       <div className="App">
         <Main
-          notes={this.state.notes}
-          currentNote={this.state.currentNote}
+          {...noteData}
           {...actions}
         />
       </div>
