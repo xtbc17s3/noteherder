69bc0bf920ba9374991b465759e27d96e69a48ae
Dave Strus <dave@getfretless.com>
2017-07-10 14:53:00 -0400

    Style the sign in page

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import './SignIn.css'
+import quill from './quill.svg'
+import googleLogo from './google.svg'
 import { auth, googleProvider } from './base'
 
 const SignIn = () => {
@@ -10,8 +12,22 @@ const SignIn = () => {
 
   return (
     <div className="SignIn">
-      <h1>SIGN IN</h1>
-      <button onClick={authenticate}>Sign In</button>
+      <header className="Header">
+        <img src={quill} alt="" />
+        <span className="title">Noteherder</span>
+      </header>
+      <main>
+        <h3>Hey, Nerd! You Like Notes?</h3>
+        <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
+        
+        <button
+          className="google"
+          onClick={authenticate}
+        >
+          <img src={googleLogo} alt="" />
+          Sign in with Google
+        </button>
+      </main>
     </div>
   )
 }
