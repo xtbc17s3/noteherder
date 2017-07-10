3d2a4b18baf0140a47c5c437c1bbf5e9a8769a7e
Dave Strus <dave@getfretless.com>
2017-07-10 09:51:09 -0400

    Update firebase config example

--- src/base.example.js
+++ src/base.example.js
@@ -1,6 +1,7 @@
 import Rebase from 're-base'
 import firebase from 'firebase/app'
 import database from 'firebase/database'
+import 'firebase/auth'
 
 const app = firebase.initializeApp({
   apiKey: "YOUR API KEY",
@@ -12,5 +13,8 @@ const app = firebase.initializeApp({
 })
 
 const db = database(app)
+
+export const auth = app.auth()
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
 
 export default Rebase.createClass(db)
