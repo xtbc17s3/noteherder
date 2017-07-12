37c0c48f3200e708b3fe498f659978621d89a83f
Dave Strus <dave@getfretless.com>
2017-07-12 14:26:56 -0400

    Add a default route in App

--- src/App.js
+++ src/App.js
@@ -142,6 +142,11 @@ class App extends Component {
                 : <Redirect to="/sign-in" />
             )}
           />
+          <Route render={() => (
+            this.signedIn()
+              ? <Redirect to="/notes" />
+              : <Redirect to="/sign-in" />
+          )} />
         </Switch>
       </div>
     )
