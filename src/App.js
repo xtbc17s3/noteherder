d5eb6d526607222fc56a55319962cb5d64100f18
Dave Strus <dave@getfretless.com>
2017-07-12 14:03:49 -0400

    Add routes for SignIn and Main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 
 import './App.css'
 import base, { auth } from './base'
@@ -121,11 +122,18 @@ class App extends Component {
 
     return (
       <div className="App">
-        {
-          this.signedIn()
-          ? <Main {...noteData} {...actions} />
-          : <SignIn />
-        }
+        <Switch>
+          <Route path="/sign-in" component={SignIn} />
+          <Route
+            path="/notes"
+            render={() => (
+              <Main
+                {...actions}
+                {...noteData}
+              />
+            )}
+          />
+        </Switch>
       </div>
     )
   }
