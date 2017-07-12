ec5926e98719c054bf33b6dd981936d9d339be7e
Dave Strus <dave@getfretless.com>
2017-07-12 14:22:17 -0400

    Redirect based on authentication status

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import './App.css'
 import base, { auth } from './base'
@@ -123,14 +123,23 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <Route path="/sign-in" component={SignIn} />
+          <Route
+            path="/sign-in"
+            render={() => (
+              this.signedIn()
+                ? <Redirect to="/notes" />
+                : <SignIn />
+            )}
+          />
           <Route
             path="/notes"
             render={() => (
-              <Main
-                {...actions}
-                {...noteData}
-              />
+              this.signedIn()
+                ? <Main
+                    {...actions}
+                    {...noteData}
+                  />
+                : <Redirect to="/sign-in" />
             )}
           />
         </Switch>
