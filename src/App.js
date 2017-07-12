81f3b0d45f439a107c6ff304310beb59f5840b7c
Dave Strus <dave@getfretless.com>
2017-07-12 14:52:57 -0400

    Redirect appropriately after save

--- src/App.js
+++ src/App.js
@@ -57,14 +57,20 @@ class App extends Component {
   }
 
   saveNote = (note) => {
-    const notes = {...this.state.notes}
+    let shouldRedirect = false
     if (!note.id) {
       note.id = Date.now()
+      shouldRedirect = true
     }
+
+    const notes = {...this.state.notes}
     notes[note.id] = note
 
     this.setState({ notes })
-    this.setCurrentNote(note)
+
+    if (shouldRedirect) {
+      this.props.history.push(`/notes/${note.id}`)
+    }
   }
 
   removeCurrentNote = () => {
