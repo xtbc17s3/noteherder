b10dfbfcba3dbc7336bf96e186c5a8d8285a69ec
Dave Strus <dave@getfretless.com>
2017-07-12 09:46:03 -0400

    Add a route that includes a note id param

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 
 import './Main.css'
 import Sidebar from './Sidebar'
@@ -6,6 +7,13 @@ import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
 const Main = (props) => {
+  const formProps = {
+    notes: props.notes,
+    currentNoteId: props.currentNoteId,
+    saveNote: props.saveNote,
+    removeNote: props.removeNote,
+  }
+
   return (
     <div className="Main">
       <Sidebar
@@ -16,12 +24,27 @@ const Main = (props) => {
         notes={props.notes}
         setCurrentNoteId={props.setCurrentNoteId}
       />
-      <NoteForm
-        notes={props.notes}
-        currentNoteId={props.currentNoteId}
-        saveNote={props.saveNote}
-        removeNote={props.removeNote}
-      />
+
+      <Switch>
+        <Route
+          path="/notes/:id"
+          render={navProps => (
+            <NoteForm
+              {...formProps}
+              {...navProps}
+            />
+          )}
+        />
+
+        <Route
+          render={navProps => (
+            <NoteForm
+              {...formProps}
+              {...navProps}
+            />
+          )}
+        />
+      </Switch>
     </div>
   )
 }
